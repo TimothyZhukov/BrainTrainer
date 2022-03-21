41662500f5f53a38f61e3fa1330e10f5f2583e59
TimothyZhukov <TimaZhukov096666096>
2022-03-21 20:57:00 +0200

    check wrong and right answer

--- app/src/main/java/com/tz/braintrainer/MainActivity.java
+++ app/src/main/java/com/tz/braintrainer/MainActivity.java
@@ -86,6 +86,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickAnswer(View view) {
+        TextView textView = (TextView) view;
+        String answer = textView.getText().toString();
+        int chosenAnswer = Integer.parseInt(answer);
+            if (chosenAnswer == rightAnswer) {
+                Toast.makeText(this, "Right!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Wrong!", Toast.LENGTH_SHORT).show();
+            }
         playNext();
+        }
     }
-}
